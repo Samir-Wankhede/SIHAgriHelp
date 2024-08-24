c12e8ec477a148414a8b71eed9259f4dcbbf0776
B Shrinidhi <b.shrinidhi1214@gmail.com>
2024-08-24 16:52:19 +0530

    dashboardRoutes configured

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import dashboardRoutes from "./routes/dashboard/dashboardRoutes"
 
 dotenv.config()
 const app = express()
 app.use(cors())
 
+app.use('/dashboard', dashboardRoutes )
 app.listen(process.env.PORT,()=>{
     console.log("Backend server listening")
 })
