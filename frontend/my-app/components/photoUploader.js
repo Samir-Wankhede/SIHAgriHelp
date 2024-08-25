3e5acef0e0a5997b62ec7252ab9ac01e296e811a
Samir Wankhede <samirsw04@gmail.com>
2024-08-25 23:01:47 +0530

    CSS fixed in prediction

--- frontend/my-app/components/photoUploader.js
+++ frontend/my-app/components/photoUploader.js
@@ -8,7 +8,7 @@ const PhotoUploader = () => {
     const cameraRef = useRef(null);
     const [filename, setFilename] = useState("")
     const [plant, setPlant] = useState()
-    const [resp, setResp] = useState()
+    const [resp, setResp] = useState(null)
     const [Img,setImg] = useState("")
     const [camOn,setCamOn] = useState(false)
     const videoConstraints = {
@@ -61,7 +61,7 @@ const PhotoUploader = () => {
         }
        }
     return (
-        <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="h-screen w-screen flex flex-col justify-center items-center bg-slate-800">
         {   !resp?
             <div className=" absolute h-[80vh] md:h-[60vh] w-[95vw] md:w-[80vw] bg-white rounded-lg grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1">
                 <div className=" col-span-1 h-full w-full bg-transparent flex justify-center items-center ">
@@ -79,11 +79,14 @@ const PhotoUploader = () => {
                     </div>
                     <img src={(Img=="")?Logo.src:Img} alt='Plant Pic'  className={`h-auto w-auto p-4 max-w-full max-h-full ${!camOn?'visible':'invisible'}`}/>     
                 </div>
-                <div className="col-span-1 h-full w-full bg-green-400 flex flex-col justify-center items-center p-2 md:p-8 rounded-lg">
-                    <div className="relative">
-                        <div className='flex flex-col gap-1'>
-                            <label className="w-full bg-white rounded-lg text-green-400 px-8 py-4">{(filename=="")?"No Picture chosen yet":filename}</label>
-                            <label htmlFor='picture' className='border border-white px-4 py-2 text-center md:w-1/2 w-full rounded-lg hover:bg-white hover:text-green-400'>
+                <div className="col-span-1 h-full w-full bg-gray-900 flex flex-col justify-center items-center p-8 rounded-lg gap-2 md:gap-5">
+                    <div className="relative w-full">
+                        <input onChange={(e)=>setPlant(e.target.value)} placeholder="Enter plant name" className="w-full bg-white rounded-lg text-gray-900 px-8 py-4"/>
+                    </div>
+                    <div className="relative w-full">
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                            <label className="w-full bg-white rounded-lg text-gray-900 px-8 py-4">{(filename=="")?"No Picture chosen yet":filename}</label>
+                            <label htmlFor='picture' className='border border-white px-8 py-2 text-white text-center w-full rounded-lg hover:bg-white hover:text-gray-900 transition-all duration-200'>
                                 Upload Picture
                             </label>  
                         </div>
@@ -93,28 +96,26 @@ const PhotoUploader = () => {
                         type="file"
                         placeholder="Choose Profile Picture"
                         id='picture'
-                        className=" opacity-0"
+                        className=" opacity-0 absolute"
                         />
                     </div>
                     <div className=" text-white text-2xl font-thin">
                         OR
                     </div>
-                    <div className="relative">
-                        <button onClick={!camOn?()=>setCamOn(true):capture} className="w-full bg-white rounded-lg text-green-400 px-8 py-4 my-5"> {!camOn?'Capture Image':'Click'} </button>
+                    <div className="relative w-full">
+                        <button onClick={!camOn?()=>setCamOn(true):capture} className="w-full bg-white rounded-lg text-gray-900 px-8 py-4"> {!camOn?'Capture Image':'Click'} </button>
                     </div>
-                    <div className="relative">
-                        <button disabled={!Img} onClick={handleSubmit} className="w-full bg-white rounded-lg text-green-400 px-8 py-4 my-5"> Find Health! </button>
-                    </div>
-                    <div className="relative">
-                        <input onChange={(e)=>setPlant(e.target.value)} placeholder="Enter plant name" className="w-full bg-white rounded-lg text-green-400 px-8 py-4 my-5"/>
+                    <div className="relative flex flex-col justify-center items-center">
+                        <button id="submitBtn" disabled={!Img} onClick={handleSubmit} className="w-full bg-white disabled:bg-slate-800 disabled:text-white rounded-lg text-gray-900 px-8 py-4 disabled:animate-pulse"> Find Health!</button>
+                        <label htmlFor="submitBtn" className={`text-white transition-opacity duration-200 ${Img ? 'opacity-0' : 'opacity-100'}`}> Select an image to activate submit!</label>
                     </div>
                 </div>
             </div>
             :
-            <div>
-            {
-                "Got resp"
-            }
+            <div className="absolute h-[80vh] md:h-[60vh] w-[95vw] md:w-[80vw] flex flex-col items-center justify-center bg-gray-900 rounded-lg">
+                <label className="text-white text-2xl">Status: {resp.status}</label>
+                <label className="text-white text-2xl">Confidence in prediction: {resp.confidence}</label>
+                <button onClick={()=>setResp(null)} className="w-full bg-white disabled:bg-slate-800 disabled:text-white rounded-lg text-gray-900 px-8 py-4 disabled:animate-pulse">Check Again!</button>
             </div>
         }
         </div>
